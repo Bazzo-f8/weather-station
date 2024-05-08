938f4efca55d44c02112f0ff553a07ce5ef19130
Michele <m.boni2000@gmail.com>
2024-05-08 12:51:13 +0200

    🔨Alcune modifiche e miglioramenti

--- weather-frontend/src/components/loginPopUp.vue
+++ weather-frontend/src/components/loginPopUp.vue
@@ -1,86 +1,88 @@
 <template>
-  <div>
-  <q-card square bordered class="q-pa-lg shadow-1">
-    <q-card-section>
-      <q-form class="q-gutter-md">
-        <q-input
-          square
-          filled
-          clearable
-          v-model="email"
-          type="email"
-          label="email"
-          lazy-rules
-          :rules="[required, isEmail]"
-        ><template v-slot:prepend> <q-icon name="mail" /> </template
-        ></q-input>
-        <q-input
-          square
-          filled
-          clearable
-          v-model="password"
-          :type="passwordFieldType"
-          label="password"
-          lazy-rules
-          :rules="[required, tooShort, includeNumber]"
-        >
-          <template v-slot:prepend> <q-icon name="lock" /> </template
-          ><template v-slot:append>
-          <q-icon :name="visibilityIcon" @click="switchVisibility" />
-        </template>
-        </q-input>
-        <q-input
-          v-if="RegIf"
-          square
-          filled
-          clearable
-          v-model="confirmPassword"
-          :type="passwordFieldType"
-          label="confirm password"
-          lazy-rules
-          :rules="[required, checkPassword]"
-        >
-          <template v-slot:prepend> <q-icon name="lock" /> </template
-          ><template v-slot:append>
-          <q-icon :name="visibilityIcon" @click="switchVisibility" />
-        </template>
-        </q-input>
-      </q-form>
-    </q-card-section>
-    <q-card-actions class="q-px-md">
-      <q-btn
-        unelevated
-        color="purple-4 "
-        size="lg"
-        class="full-width"
-        :label="nameBot"
-        @click="submit"
-      />
-    </q-card-actions>
-    <q-card-section class="text-center q-pa-none">
-      <q-btn
-        no-caps
-        flat
-        color="purple 6"
-        @click="changeToReg"
-        label="non sei loggato? registrati"
-      />
-      <q-btn
-        no-caps
-        unelevated
-        rounded
-        color="purple 1"
-        @click="whitoutLog"
-        label="continua senza loggare"
-      />
-    </q-card-section>
-  </q-card>
+  <div class="q-pa-md">
+    <q-card square bordered class="q-pa-lg shadow-1">
+      <q-card-section>
+        <q-form class="q-gutter-md">
+          <q-input
+            square
+            filled
+            clearable
+            v-model="email"
+            type="username"
+            label="username"
+            lazy-rules
+            :rules="[required]"
+            ><template v-slot:prepend> <q-icon name="person" /> </template
+          ></q-input>
+          <q-input
+            square
+            filled
+            clearable
+            v-model="password"
+            :type="passwordFieldType"
+            label="password"
+            lazy-rules
+            :rules="[required]"
+          >
+            <template v-slot:prepend> <q-icon name="lock" /> </template
+            ><!--visualizza password/non visualizzare-->
+            <template v-slot:append>
+              <q-icon :name="visibilityIcon" @click="switchVisibility" />
+            </template>
+          </q-input>
+          <q-input
+            v-if="RegIf"
+            square
+            filled
+            clearable
+            v-model="confirmPassword"
+            :type="passwordFieldType"
+            label="confirm password"
+            lazy-rules
+            :rules="[required, checkPassword]"
+          >
+            <template v-slot:prepend> <q-icon name="lock" /> </template
+            ><template v-slot:append>
+              <q-icon :name="visibilityIcon" @click="switchVisibility" />
+            </template>
+          </q-input>
+        </q-form>
+      </q-card-section>
+      <q-card-actions class="q-px-md">
+        <q-btn
+          unelevated
+          color="blue"
+          size="lg"
+          class="full-width"
+          :label="nameBot"
+          @click="submit"
+        />
+      </q-card-actions>
+      <q-card-section class="text-center q-pa-none">
+        <q-btn
+          no-caps
+          flat
+          color="black"
+          @click="changeToReg"
+          label="Non sei loggato? Registrati ✅"
+        />
+        <q-btn
+          no-caps
+          unelevated
+          rounded
+          color="blue 1"
+          @click="whitoutLog"
+          label="continua senza loggare"
+        />
+      </q-card-section>
+    </q-card>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 import { useRouter } from "vue-router";
+import axios from "src/boot/axios";
 
 const router = useRouter();
 const confirmPassword = ref("");
@@ -90,28 +92,16 @@ const RegIf = ref(false);
 const nameBot = ref("Login");
 const visibilityIcon = ref("visibility");
 const passwordFieldType = ref("password");
-//controllo input-------------------------------------------------------------------------------------------------------------------------------------
+
+//controllo input
 const required = (val) => {
   return (val && val.length > 0) || "insert something";
-};
-
-const isEmail = (val) => {
-  const emailPattern =
-    /^(?=[a-zA-Z0-9@._%+-]{6,254}$)[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/;
-  return emailPattern.test(val) || "insert a valid Email address";
-};
-
-const tooShort = (val) => {
-  return (val && val.length > 3) || "you have to use at least 3 characters";
-};
-
-const includeNumber = (val) => {
-  return (val && /\d/.test(val)) || "you have to use at least one number";
 };
 const checkPassword = (val) => {
   return (val && val === password.value) || "password doesn't match";
 };
-//-------------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------
+//Login page to -> Registration Page
 const changeToReg = () => {
   RegIf.value = !RegIf.value;
   if (RegIf.value === false) {
@@ -120,7 +110,8 @@ const changeToReg = () => {
     nameBot.value = "Register";
   }
 };
-
+//------------------------------------------------------------
+//visualizza password
 const switchVisibility = () => {
   if (passwordFieldType.value === "password") {
     passwordFieldType.value = "text";
@@ -130,9 +121,18 @@ const switchVisibility = () => {
     visibilityIcon.value = "visibility";
   }
 };
-
-const submit = () => {
-  return 0;
+//collegamento back end
+const submit = async () => {
+  try {
+    const response = await axios.post("url back end", {
+      email: email.value,
+      password: password.value,
+    });
+    localStorage.setItem();
+    router.push("/favorite");
+  } catch (err) {
+    console.log("errore durante il login");
+  }
 };
 //-------------------------------------------------------------------------------------------------------------------------------------
 const whitoutLog = () => {
@@ -142,6 +142,7 @@ const whitoutLog = () => {
 
 <style>
 .q-card {
-  width: 360px;
+  width: 400px;
+  margin: auto;
 }
 </style>
